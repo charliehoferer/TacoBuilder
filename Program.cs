24408bff322b699d4b2f23fcba101bd99da481f4
Charlie Hoferer <charlie.hoferer@gmail.com>
2021-03-25 12:50:10 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TacoBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tacoQty = 12;
+            string tacoToppings = "Beef, Cheese, Lettuce, and Tomato";
+                      
+            Console.WriteLine($"You have {tacoQty} tacos, and they are filled with {tacoToppings}.");
+                
+            
+        }
+                
+            
+        
+    }
+}
